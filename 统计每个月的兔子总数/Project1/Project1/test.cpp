2915f48a7c58cca9af95c0660ad6bd9cb40b62e6
王一帆 <943550207@qq.com>
2019-06-09 15:37:35 +0800

    Add files via upload

---
+++ 统计每个月的兔子总数/Project1/Project1/test.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int help(int n)
+{
+	if (n == 1 || n == 2)
+		return 1;
+	return help(n - 1) + help(n - 2);
+}
+int help2(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c;
+	for (int i = 2; i<n; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return c;
+}
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		if (n > 2)
+		{
+			int ret = help2(n);
+			cout << ret << endl;
+		}
+		else
+		{
+			int ret = help(n);
+			cout << ret << endl;
+		}
+
+
+	}
+
+}
